24aa673e50c406037d816adbfd01c79d5c9c1e5d
Erin V <erinkvoelker@gmail.com>
2022-09-06 18:33:02 -0400

    adding plural helper

---
+++ utils/helpers.js
@@ -0,0 +1,32 @@
+module.exports = {
+  format_date: (date) => {
+    // Format date as MM/DD/YYYY
+    return date.toLocaleDateString();
+  },
+  format_plural: (word, amount) => {
+    if (amount!==1) {
+      return `${word}s`;
+    }
+    return word;
+  },
+  format_amount: (amount) => {
+    // format large numbers with commas
+    return parseInt(amount).toLocaleString();
+  },
+  get_emoji: () => {
+    const randomNum = Math.random();
+
+    // Return a random emoji
+    if (randomNum > 0.7) {
+      return '<span for="img" aria-label="lightbulb">💡</span>';
+    } else if (randomNum > 0.4) {
+      return '<span for="img" aria-label="laptop">💻</span>';
+    } else {
+      return '<span for="img" aria-label="gear">⚙️</span>';
+    }
+  },
+  // allows handlebars to if the session user matches the post or comment user
+  same_user: (sess_user, list_user) => {
+    return sess_user == list_user;
+  },
+};
