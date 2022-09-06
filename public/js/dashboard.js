a3c90b254c501df182ce9d35889d4e3847787451
Erin V <erinkvoelker@gmail.com>
2022-09-06 18:55:18 -0400

    adding upvotes to post get route

---
+++ public/js/dashboard.js
@@ -0,0 +1,47 @@
+// create new post
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#post-title').value.trim();
+  const contents = document.querySelector('#post-cont').value.trim();
+
+  if (title && contents) {
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({ title, contents }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create post');
+    }
+  }
+};
+// delete post
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+document
+  .querySelector('.new-post-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.post-list')
+  .addEventListener('click', delButtonHandler);
